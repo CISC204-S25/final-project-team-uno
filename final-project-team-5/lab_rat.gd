47b1f6830e0c0a31d9a2cfcb0f89adf123a21ecb
Leem <114524149+agentLeem@users.noreply.github.com>
2025-04-29 12:04:33 -0400

    Basic Character Created

--- final-project-team-5/lab_rat.gd
+++ final-project-team-5/lab_rat.gd
@@ -1,25 +1,21 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+@export var speed = 300.0
+@export var jumpVelocity = -400.0
+var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
-		velocity += get_gravity() * delta
-
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+		velocity.y += gravity * delta
+	
+	#inputs only affect x axis
+	velocity.x = Input.get_axis("move_left", "move_right") * speed
+	
 	move_and_slide()
+	
+	#only jump when on the ground
+	if Input.is_action_pressed("jump") and is_on_floor():
+		velocity.y = jumpVelocity
