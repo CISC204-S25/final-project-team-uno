4cc2382cd53dc206a7d3aa31917bd057c74eb252
Leem <114524149+agentLeem@users.noreply.github.com>
2025-05-02 14:05:37 -0400

    Updated Code thumbs up emoji

--- final-project-team-5/Scripts/lab_rat.gd
+++ final-project-team-5/Scripts/lab_rat.gd
@@ -4,6 +4,8 @@ extends CharacterBody2D
 @export var speed = 300.0
 @export var jumpVelocity = -400.0
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
+@export_range(0.0, 1.0) var friction = 0.1
+@export_range(0.0 , 1.0) var acceleration = 0.25
 
 
 func _physics_process(delta: float) -> void:
@@ -12,7 +14,14 @@ func _physics_process(delta: float) -> void:
 		velocity.y += gravity * delta
 	
 	#inputs only affect x axis
-	velocity.x = Input.get_axis("move_left", "move_right") * speed
+	var dir = Input.get_axis("walk_left", "walk_right")
+	
+	#applies driction and acceleration
+	if dir != 0:
+		velocity.x = lerp(velocity.x, dir * speed, acceleration)
+	else:
+		velocity.x = lerp(velocity.x, 0.0, friction)
+
 	
 	move_and_slide()
 	
