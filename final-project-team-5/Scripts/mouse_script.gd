55d6617621ac731de4f1aee84958d5612d06761d
KamPit0605 <kampitud@udel.edu>
2025-05-07 14:56:51 -0400

    Platform Can Move & Mouse Comments
    Added a boolean that allows the platform to move (only to the right as of right now, and there is no toggle other than turning the bool true or false in the Inspector window).
    
    Added Comments to the mouse_script.gd

--- final-project-team-5/Scripts/mouse_script.gd
+++ final-project-team-5/Scripts/mouse_script.gd
@@ -18,7 +18,7 @@ func _process(delta):
 
 func _platformcloning():
 	if Input.is_action_just_pressed("left_click"):
-		var clonedPlatform = platform.instantiate()
-		target = get_global_mouse_position()
-		clonedPlatform.position = target
+		var clonedPlatform = platform.instantiate() #sets platform (scene) to its 
+		target = get_global_mouse_position() #target is set to the mouse's position
+		clonedPlatform.position = target #sets the created platform to that position with a left_click
 		add_child(clonedPlatform)
--- final-project-team-5/Scripts/platform_script.gd
+++ final-project-team-5/Scripts/platform_script.gd
@@ -1,5 +1,24 @@
 extends StaticBody2D
 
+@export var moving_platform: bool = true
+@export var speed = 200
+@export var TimeB4Moving: Timer
+var movetheplatform: bool = false
+
+func _physics_process(delta):
+	if moving_platform == true:
+		#TimeB4Moving.start()
+		pass
+		if movetheplatform == true:
+			move_local_x(2.5, true) #moves the platform after time delay
+
+func _ready():
+	if moving_platform == true:
+		TimeB4Moving.start()
 
 func _on_platform_duration_timeout():
-	queue_free()
+	queue_free() #when time is up, the platform is deleted
+
+
+func _on_time_before_moving_timeout():
+	movetheplatform = true
