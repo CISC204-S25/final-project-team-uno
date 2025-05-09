8446a563461ba943bdb3f228ec6db1418a675af7
KamPit0605 <kampitud@udel.edu>
2025-05-09 17:41:54 -0400

    Altered Buddy Script
    Changed a few things in the "lab_rat.gd" script. Added a new exported-variable to increase the gravity force by multiplying. Also created a "different_test" script to test the altered physics (also shrunk Buddy in the scene).

--- final-project-team-5/Scripts/lab_rat.gd
+++ final-project-team-5/Scripts/lab_rat.gd
@@ -7,21 +7,23 @@ var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 @export_range(0.0, 1.0) var friction = 0.1
 @export_range(0.0 , 1.0) var acceleration = 0.25
 
+#fixes buddy's gravity (velocity in the y-axis). Buddy can no longer fly high or be floaty >:( (or Buddy can again, just change it back to 0.)
+@export var gravityPull = 50 
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
-		velocity.y += gravity * delta
+		velocity.y += gravity * gravityPull * delta
 	
 	#inputs only affect x axis
 	var dir = Input.get_axis("move_left", "move_right")
 	
-	#applies driction and acceleration
+	#applies friction and acceleration
 	if dir != 0:
 		velocity.x = lerp(velocity.x, dir * speed, acceleration)
 	else:
 		velocity.x = lerp(velocity.x, 0.0, friction)
-
+		
 	
 	move_and_slide()
 	
